de8346bcfec07cabf31fd8a6ae8df5f31201c5c1
Cernurt <60700705+Cernurt@users.noreply.github.com>
2023-02-23 00:40:24 +0100

    Avancement sur PWM
    Fonction pour calculer vitesse en continue, obtenir le nbr de tour/min
    
    Probleme = Une seule des broches est utilisée, j'arrive pas à utiliser l'ENCODEURB qui est inutile dans ce code.

--- Motorisation/PWM/PWM.ino
+++ Motorisation/PWM/PWM.ino
@@ -1,4 +1,5 @@
 #include <AFMotor.h>
+#include <TimerOne.h>
 
 #define ENCODEURA 19
 #define ENCODEURB 38
@@ -6,12 +7,18 @@
 AF_DCMotor motor(4); // Initalisation du moteur branchements sur M4
 uint8_t i;
 int hallticks;
+double speed = 0;
+
+double tickperround = 1400; // Valeur à changer en fonction du capteur à effet hall
  
 void setup() {
- pinMode(ENCODEURA, INPUT_PULLUP);
- pinMode(ENCODEURB, INPUT_PULLUP);
+  pinMode(ENCODEURA, INPUT_PULLUP);
+  pinMode(ENCODEURB, INPUT_PULLUP);
  
- attachInterrupt(digitalPinToInterrupt(19), test, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(19), test, CHANGE);
+
+  Timer1.initialize(100000); // On défini le timeur : 50000 microseconds ( 0.05 sec - or 20Hz )
+  Timer1.attachInterrupt( timerSpeed ); 
 
 
   
@@ -29,13 +36,26 @@ void loop() {
   Serial.print("Tourne en avant - ");
   
   motor.run(FORWARD);
-  motor.setSpeed(200);
-  delay(670);     // Delai a ajuster pour que ca fasse pile un tour
-  Serial.println(hallticks);
-  motor.run(RELEASE);
-  hallticks = 0;
-  delay(4000);
+  motor.setSpeed(110);
 
+
+  
+  delay(5000);
+  Serial.println("-------------Debut tour-----------------");
+  delay(1000);
+  Serial.println("---------------Fin tour------------------");
+  motor.run(RELEASE);
+  delay(10000);
+
+}
+
+void timerSpeed(){
+  //Serial.println(hallticks);
+  speed = (hallticks/tickperround)/0.1;
+  Serial.print("Speed = ");
+  Serial.println(speed);
+  
+  hallticks = 0;
 }
 
 void test(){
