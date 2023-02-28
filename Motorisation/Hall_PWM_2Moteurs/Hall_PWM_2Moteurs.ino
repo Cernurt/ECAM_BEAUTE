78986ed449eae163f0d5cac77df8867fdfba9b9c
Cernurt <60700705+Cernurt@users.noreply.github.com>
2023-02-28 17:12:52 +0100

    Update Hall_PWM_2Moteurs.ino

--- Motorisation/Hall_PWM_2Moteurs/Hall_PWM_2Moteurs.ino
+++ Motorisation/Hall_PWM_2Moteurs/Hall_PWM_2Moteurs.ino
@@ -15,11 +15,23 @@ C'est important que ce soit la PIN 19 et 18 et pas une autre car c'est une PIN d
 
 AF_DCMotor motor_R(3, MOTOR34_8KHZ); // Initalisation du moteur branchements sur M2
 AF_DCMotor motor_L(2, MOTOR12_8KHZ); // Initalisation du moteur branchements sur M1
-uint8_t i;
 int hallTicksR;
 int hallTicksL;
 double speedR = 0;
 double speedL = 0;
+
+double cibleVitesseR;
+double cibleVitesseL;
+double cibleVitesse;
+
+double DeltaR;
+double DeltaL;
+
+
+int rightMotorPWM = 0;
+int leftMotorPWM = 0;
+//char *var_RightMotor[] = {"FORWARD", "255"};
+//char *var_LeftMotor[] = {"FORWARD", "255"};
 
 double tickperround = 600; // Valeur à changer en fonction du capteur à effet hall
  
@@ -34,19 +46,83 @@ void setup() {
   Timer1.initialize(100000); // On défini le timeur : 50000 microseconds ( 0.05 sec - or 20Hz )
   Timer1.attachInterrupt( timerSpeed ); 
 
-  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.begin(9600);           // set up Serial library at 9600 
+
+  motor_R.run(FORWARD);
+  motor_L.run(FORWARD);
+  
+  cibleVitesse = 2.00;
+  ToutDroitCapitaine();
 }
+
+
+
+
 
 void loop() {
- 
-  motor_R.run(FORWARD);
-  motor_R.setSpeed(150);
-  motor_L.run(FORWARD);
-  motor_L.setSpeed(150);
 
+  
+  DeltaR = cibleVitesseR - speedR;
+  if (DeltaR != 0){
+    if ((DeltaR > 0 ) && (rightMotorPWM <  255)){
+      rightMotorPWM += map(DeltaR, 0, cibleVitesseR, 0.9, 10);
+      motor_R.setSpeed(rightMotorPWM);
+    }
+    if ((DeltaR < 0) && (rightMotorPWM > 0)){
+      rightMotorPWM -= 2;
+      motor_R.setSpeed(rightMotorPWM);
+    }
+  }
+
+  DeltaL = cibleVitesseL - speedL;
+  if (DeltaL != 0){
+    if ((DeltaL > 0) && (leftMotorPWM <  255)){
+      leftMotorPWM += map(DeltaL, 0, cibleVitesseL, 0.9, 10);;
+      motor_L.setSpeed(leftMotorPWM);
+    }
+    if ((DeltaL < 0) && (leftMotorPWM > 0)) {
+      leftMotorPWM -= 2;
+      motor_L.setSpeed(leftMotorPWM);
+    }
+  }
+
+
+
+/*
+  while((speedR <= cibleVitesseR) || (speedL <= cibleVitesseL)){
+    if (rightMotorPWM < 255) {
+      rightMotorPWM += 1;
+      Serial.println(rightMotorPWM);
+      motor_R.setSpeed(rightMotorPWM);
+    }
+    if (leftMotorPWM < 255){
+      leftMotorPWM += 1;
+      Serial.println(leftMotorPWM);
+      motor_L.setSpeed(leftMotorPWM);
+    }
+    delay(10);
+  }
+*/
+
+ 
+  
+  
+  delay(200);
 }
 
 
+
+
+
+
+
+
+
+
+void ToutDroitCapitaine(){
+  cibleVitesseR = cibleVitesse;
+  cibleVitesseL = cibleVitesse;
+}
 
 void timerSpeed(){
   //Serial.println(hallTicksR);
