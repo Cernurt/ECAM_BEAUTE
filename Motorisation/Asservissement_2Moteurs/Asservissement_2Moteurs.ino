6738228ff11022cf1e4d3ccdb0579d8b9d2f95fd
Cernurt <60700705+Cernurt@users.noreply.github.com>
2023-03-10 10:12:19 +0100

    Jsp

---
+++ Motorisation/Asservissement_2Moteurs/Asservissement_2Moteurs.ino
@@ -0,0 +1,162 @@
+/*
+Ce programme calcule la vitesse de rotation de deux moteur en arrière plan pendant que le loop exécute n'importe quelle action sur les varibales cibleVitesse.
+Asservissement en vitesse de rotation sur chaque moteur séparément
+Le shield doit etre branche sur la MEGA comme un debile, le moteur est alimenté par le shield, le capteur Hall est en +5V et GND sur la arduino et un seul de ses capteur est utilisé, n'importe lequel, et il doit etre branche sur la pin 19
+C'est important que ce soit la PIN 19 et 18 et pas une autre car c'est une PIN dInterrupt.
+
+
+*/
+
+#include <AFMotor.h>
+#include <TimerOne.h>
+
+#define ENCODEURA 19
+//#define ENCODEURB 38
+#define ENCODEURC 18
+
+AF_DCMotor motor_R(3, MOTOR34_8KHZ); // Initalisation du moteur branchements sur M2
+AF_DCMotor motor_L(2, MOTOR12_8KHZ); // Initalisation du moteur branchements sur M1
+volatile int hallTicksR;
+volatile int hallTicksL;
+int ttlTicksR = 0;
+int ttlTicksL = 0;
+
+
+double speedR = 0;
+double speedL = 0;
+
+double cibleVitesseR;
+double cibleVitesseL;
+double cibleVitesse;
+
+double speedDeltaR;
+double speedDeltaL;
+
+
+double rightMotorPWM = 0;
+double leftMotorPWM = 0;
+//char *var_RightMotor[] = {"FORWARD", "255"};
+//char *var_LeftMotor[] = {"FORWARD", "255"};
+boolean AsservTrankil;
+
+double tickperround = 1400; // Valeur à changer en fonction du capteur à effet hall
+ 
+void setup() {
+  pinMode(ENCODEURA, INPUT_PULLUP);
+  pinMode(ENCODEURC, INPUT_PULLUP);
+  //pinMode(ENCODEURB, INPUT_PULLUP);
+ 
+  attachInterrupt(digitalPinToInterrupt(19), compteurR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(18), compteurL, CHANGE);
+
+  Timer1.initialize(50000); // On défini le timeur : 50000 microseconds ( 0.05 sec - or 20Hz )
+  Timer1.attachInterrupt( timerSpeed ); 
+
+
+  Serial.begin(115200);           // set up Serial library at 115200
+
+  motor_R.run(FORWARD);
+  motor_L.run(FORWARD);
+  
+  cibleVitesse = 0;
+  ToutDroitCapitaine();
+}
+
+
+
+
+
+void loop() {
+
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+void ToutDroitCapitaine(){
+  cibleVitesseR = cibleVitesse;
+  cibleVitesseL = cibleVitesse;
+}
+
+void MotorAsserv(){
+
+  // Motor Right
+  
+   speedDeltaR = cibleVitesseR - speedR;
+  if (speedDeltaR != 0){
+    if ((speedDeltaR > 0 ) && (rightMotorPWM <  255)){
+      rightMotorPWM += map(speedDeltaR, 0, cibleVitesseR, 1, 4);
+      motor_R.setSpeed(int(round(rightMotorPWM)));
+    }
+
+    
+    if ((speedDeltaR < 0) && (rightMotorPWM > 0)){
+      Serial.println("Motor Right ralenti.......");
+      rightMotorPWM -= map(speedDeltaR, 0, cibleVitesseR, 1, 10);
+      motor_R.setSpeed(int(round(rightMotorPWM)));
+    }
+  }
+
+  // Motor Left
+
+   speedDeltaL = cibleVitesseL - speedL;
+
+  if (speedDeltaL != 0){
+    if ((speedDeltaL > 0 ) && (leftMotorPWM <  255)){
+      leftMotorPWM += map(speedDeltaL, 0, cibleVitesseL, 1, 4);
+      motor_L.setSpeed(int(round(leftMotorPWM)));
+    }
+
+    
+    if ((speedDeltaL < 0) && (leftMotorPWM > 0)){
+      Serial.println("Motor Left ralenti.......");
+      leftMotorPWM -= map(speedDeltaL, 0, cibleVitesseL, 1, 10);
+      motor_L.setSpeed(int(round(leftMotorPWM)));
+    }
+  }
+  
+  
+}
+
+
+
+
+
+void timerSpeed(){
+  //Serial.println(hallTicksR);
+  if (!(AsservTrankil)){
+    MotorAsserv();
+    AsservTrankil = true;
+  } else {
+    AsservTrankil = false;
+  }
+    
+  
+  speedR = (hallTicksR/tickperround)/0.05; // Le 0.1 doit correspondre a la fréquence de relevé de speed ligne 20
+  speedL = (hallTicksL/tickperround)/0.05; // Le 0.1 doit correspondre a la fréquence de relevé de speed ligne 20
+  Serial.println(hallTicksR/0.05);
+  Serial.print("SpeedR = ");
+  Serial.print(speedR);
+  Serial.print(" | SpeedL =");
+  Serial.println(speedL);
+  ttlTicksR += hallTicksR
+  hallTicksR = 0;
+  ttlTicksL += hallTicksL
+  hallTicksL = 0;
+}
+
+void compteurL(){
+  hallTicksL = hallTicksL + 1;
+}
+
+void compteurR(){
+  hallTicksR = hallTicksR + 1;
+}
